78d0db4e96e3cbb0ba1e65577c7dd7786de5e9be
Connor Blackler <connor.blackler.ss@gmail.com>
2023-05-20 21:05:33 +0100

    [#1] {t:400} {neural-network:main} Adding backpropogation to ReLU and DenseLayer

--- neural_net/helpers/activation.py
+++ neural_net/helpers/activation.py
@@ -10,7 +10,16 @@ class Activation(ABC):
 
 class ActivationReLU(Activation):
     def forward(self, inputs):
+        self.inputs = inputs
         self.output = np.maximum(0, inputs)
+
+    def backward(self, dvalues):
+        # Since we need to modify the original variable,
+        # let's make a copy of the values first
+        self.dinputs = dvalues.copy()
+
+        # Zero gradient where input values were negative
+        self.dinputs[self.inputs <= 0] = 0
 
 
 class ActivationSoftmax(Activation):
--- neural_net/helpers/layer.py
+++ neural_net/helpers/layer.py
@@ -10,3 +10,12 @@ class DenseLayer:
 
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
+        self.inputs = inputs
+
+    def backward(self, dvalues):
+        # Gradients on parameters
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+
+        # Gradient on values
+        self.dinputs = np.dot(dvalues, self.weights.T)
