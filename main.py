d2b2ca98acf1e520c84ffbf25745a874ef9c24d1
Connor Blackler <connor.blackler.ss@gmail.com>
2023-05-20 16:34:09 +0100

    [#1] {t:120} {neural-network:main} adding two denselayers, adding softmax activation using np.exp, np.max, and np.sum for probability output

--- main.py
+++ main.py
@@ -18,8 +18,55 @@ class DenseLayer:
         self.output = np.dot(inputs, self.weights) + self.biases
 
 
+# ReLU activation
+class ActivationReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+# Softmax activation
+class ActivationSoftmax:
+    def forward(self, inputs):
+        # Get unnormalized probabilities
+        exp_values = np.exp(inputs - np.max(inputs, axis=1,
+                                            keepdims=True))
+
+        # Normalize them for each sample
+        probabilities = exp_values / np.sum(exp_values, axis=1,
+                                            keepdims=True)
+        self.output = probabilities
+
+
+# Create dataset
 X, y = spiral_data(samples=100, classes=3)
 
+# Create Dense layer with 2 input features and 3 output values
 dense1 = DenseLayer(2, 3)
+
+# Create ReLU activation (to be used with Dense layer):
+relu_activation = ActivationReLU()
+
+# Create second Dense layer with 3 input features (as we take output
+# of previous layer here) and 3 output values
+dense2 = DenseLayer(3, 3)
+
+# Create Softmax activation (to be used with Dense layer):
+softmax_activation = ActivationSoftmax()
+
+# Make a forward pass of our training data through this layer
 dense1.forward(X)
-print(dense1.output[:5])
+
+# Make a forward pass through activation function
+# it takes the output of first dense layer here
+relu_activation.forward(dense1.output)
+
+# Make a forward pass through second Dense layer
+# it takes outputs of activation function of first layer as inputs
+dense2.forward(relu_activation.output)
+
+# Make a forward pass through activation function
+# it takes the output of second dense layer here
+softmax_activation.forward(dense2.output)
+
+# Let's see output of the first few samples:
+print(softmax_activation.output[:5])
