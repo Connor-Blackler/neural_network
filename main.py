143eb604dfec51aa6310342fa055018eec397f6c
Connor Blackler <connor.blackler.ss@gmail.com>
2023-05-20 15:05:54 +0100

    [#1] {t:120} {neural-network:main} updated layer to use transpose in order to dot product two 2D matricies together

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import sys
 import numpy as np
-import matplotlib
 
+inputs = [[1.0, 2.0, 3.0, 2.5],
+          [2.0, 5.0, -1.0, 2.0],
+          [-1.5, 2.7, 3.3, -0.8]]
 
-inputs = [1, 2, 3, 2.5]
 weights = [[0.2, 0.8, -0.5, 1.0],
            [0.5, -0.91, 0.26, -0.5],
            [-0.26, -0.27, 0.17, 0.87]]
 
-biases = [2, 3, 0.5]
+biases = [2.0, 3.0, 0.5]
+layer_outputs = np.dot(inputs, np.array(weights).T) + biases
 
-output = np.dot(weights, inputs) + biases
-print(output)
+print(layer_outputs)
