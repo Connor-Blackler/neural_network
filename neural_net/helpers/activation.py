206a78393982e7e87271b766bd78054ec0ebc18e
Connor Blackler <connor.blackler.ss@gmail.com>
2023-05-20 21:26:02 +0100

    {neural-network:main} {t:120} adding backpropogation to softmax activation

--- neural_net/helpers/activation.py
+++ neural_net/helpers/activation.py
@@ -36,3 +36,19 @@ class ActivationSoftmax(Activation):
         probabilities = exp_values / np.sum(exp_values, axis=1,
                                             keepdims=True)
         self.output = probabilities
+
+    def backward(self, dvalues):
+        self.dinputs = np.empty_like(dvalues)
+        for index, (single_output, single_dvalues) in \
+                enumerate(zip(self.output, dvalues)):
+
+            single_output = single_output.reshape(-1, 1)
+
+            # Calculate Jacobian matrix of the output
+            jacobian_matrix = np.diagflat(single_output) - \
+                np.dot(single_output, single_output.T)
+
+            # Calculate sample-wise gradient
+            # and add it to the array of sample gradients
+            self.dinputs[index] = np.dot(jacobian_matrix,
+                                         single_dvalues)
